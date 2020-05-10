72eba23c9bab66f15467d5e6033c0e79a11aedb5
Saidnazar97 <lifedada98@gmail.com>
2020-05-09 18:00:03 -0700

    adding function

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,30 @@
 $(document).ready(function () {
   var inputText = "";
+
   $("#submitbtn").on("click", function (e) {
     e.preventDefault();
     inputText = $("#listSearch").val();
     $("#listSearch").val("");
     console.log(inputText);
-    $(".container").prepend(`<p>${inputText}</p>`);
+    // $("body").prepend(`<p>${inputText}</p>`);
 
     $.ajax({
       type: "GET",
-      url: `https://api.openweathermap.org/data/2.5/weather?zip=${inputText}&appid=c1dc5cf44c3a5ebaadb89b904175bcca`,
-      dataType: "json",
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${inputText}&appid=c1dc5cf44c3a5ebaadb89b904175bcca`,
+      dataType: "JSON",
     }).then(function (response) {
       console.log(response.main.temp);
       var cityName = response.name;
+
       var temp = response.main.temp;
       var tempC = temp - 273.15;
       var tempF = 1.8 * tempC + 32;
       var tempC2 = tempC.toFixed(2);
       var tempF2 = tempF.toFixed(2);
-
-      $("body").prepend(`<div>city name: ${cityName}</div>`)
-      $("body").prepend(`<div>temperature in celcius: ${tempC2} °C</div>`)
-      $("body").prepend(`<div>temperature in F: ${tempF2} °F</div>`)
+      var humidity =
+        $(".body2").append(`<div><strong>${cityName}</strong></div> </br>`)
+      // $(".body2").append(`<div>Temperature in celcius: ${tempC2} °C</div>`)
+      $(".body2").append(`<h5 class="text-muted">Temperature: ${tempF2} °F</h5>`)
 
     });
   });
